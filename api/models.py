ed518fb431c30faf08cd66f6bd1b3b0d64b5c42a
Alvaro <alvaro.nrx88@gmail.com>
2025-03-28 18:11:45 -0300

    initial commit bidworks-backend

---
+++ api/models.py
@@ -0,0 +1,3 @@
+from django.db import models
+
+#* Modelos -> user, Freela, worker _> definir modelos adjacentes baseados no diagrama
---
+++ api/views.py
@@ -0,0 +1,3 @@
+from django.shortcuts import render
+
+#* Criar algo baseado no padrão REST _> implementar segurança 
