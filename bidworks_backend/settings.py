9553a072a43c66cf136bd4ac2967f9b6173a5aca
Alvaro <alvaro.nrx88@gmail.com>
2025-03-31 15:05:54 -0300

    feat: requirements.txt fix

--- bidworks_backend/settings.py
+++ bidworks_backend/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5cpxv-7&zpmjp26jxhduaa@-g*b#k0io1(xr_h4m%c#mwd%toc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 
