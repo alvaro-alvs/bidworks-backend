fd4689db3cf3556eb00a7e9fb6184f281cdb4115
Alvaro <alvaro.nrx88@gmail.com>
2025-04-12 17:50:42 -0300

    fix -> test-production-1

--- bidworks_backend/settings.py
+++ bidworks_backend/settings.py
@@ -35,10 +35,12 @@ DEBUG = False
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
+    "bidworks-backend.vercel.app"
 ]
 
 CORS_ALLOWED_ORIGINS = [
     "127.0.0.1",
+    "bidworks-backend.vercel.app"
 ]
 
 # Application definition
